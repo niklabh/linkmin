b379eab9e923516cecdb8f68e8c9587df54b1323
Nikhil Ranjan <niklabh811@gmail.com>
2016-08-28 06:47:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,49 +10,28 @@ var http = require('http');
 var path = require('path');
 var util = require('util');
 var proc = require('./bin/proc');
-var RedisStore = require('connect-redis')(express);
 
 var app = express();
 
-// all environments
-app.configure(function () {
-	app.set('port', process.env.PORT || 3000);
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.use(express.favicon());
-	app.use(express.logger('dev'));
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.cookieParser('1a2b3c4d5e6f'));
-	app.use(express.session({
-		secret: 'niklabh@git',
-		store: new RedisStore({
-			host: process.env.REDIS_URL || '127.0.0.1'
-		}),
-		key: '123@#$12333'
-	}));
-	app.use(express.static(path.join(__dirname, 'public')));
-});
 
-// development
-app.configure('development', function () {
-	app.use(express.logger('dev'));
-	app.use(app.router);
-  app.use(express.errorHandler());
-});
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('1a2b3c4d5e6f'));
+app.use(express.session({
+	secret: 'niklabh@git',
+	key: '123@#$12333'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//production
-app.configure('production', function () {
-  express.logger.token('istDate', function (req, res) {
-    return new Date();
-  });
-  app.use(express.logger({
-    format: ':istDate :method :url :status :res[Content-Length] :response-time ms'
-  }));
-  //app.use('/', require('./routes/lib/authenticate'));
-  app.use(app.router); // use router provided by express
-  app.use(require('./routes/lib/error')); // defines route to handle errors for all requests
-});
+app.use(express.logger('dev'));
+app.use(app.router);
+app.use(express.errorHandler());
+
 
 require('./routes')(app);
 
