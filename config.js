1dcd1338d520800b39e9d2c12168e81405e0263a
Nikhil Ranjan <niklabh811@gmail.com>
2016-08-28 08:26:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-  host: 'http://linkm.in',  // Your host name
+  host: 'http://www.linkm.in',  // Your host name
   redis: {
     host: process.env.REDIS_URL || '127.0.0.1', // redis server
     port: 6379 // redis port
